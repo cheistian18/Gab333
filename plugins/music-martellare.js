edb8db5bfc50120bef49a63da2495b6a4fcba64d
cheistian18 <81108968+cheistian18@users.noreply.github.com>
2025-06-27 10:35:44 +0200

    Update music-martellare.js

--- plugins/music-martellare.js
+++ plugins/music-martellare.js
@@ -1,20 +1,43 @@
-//crediti: Onix, di Riad
-let handler = async (m, { conn }) => {
-  try {
-    // Percorso del file musicale
-    let audioPath = './storage/mp3/freestyle.mp3';
 
-    // Invia il file audio come messaggio nel gruppo
-    await conn.sendMessage(m.chat, { audio: { url: audioPath }, mimetype: 'audio/mp4', ptt: true });
-  } catch (err) {
-    console.error('𝐄𝐫𝐫𝐨𝐫𝐞 𝐧𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨❗', err);
-    await m.reply('⚠️ 𝐄𝐫𝐫𝐨𝐫𝐞');
+var linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
+var surveyRegex = /https?:\/\/(www\.)?whatsurvey\.com\/[^\s]+/i;
+
+export async function before(msg, { isAdmin, isBotAdmin }) {
+  if (msg.isBaileys && msg.fromMe) return true;
+  if (!msg.isGroup) return false;
+  var chatData = global.db.data.chats[msg.chat];
+  var sender = msg.key.participant;
+  var messageId = msg.key.id;
+  var botSettings = global.db.data.settings[this.user.jid] || {};
+
+  var foundLink = linkRegex.exec(msg.text);
+  var foundSurveyLink = surveyRegex.exec(msg.text);
+
+  if (isAdmin && chatData.antiLink && (msg.text.includes("https://chat.whatsapp.com") || foundSurveyLink)) return;
+  if (chatData.antiLink && (foundLink || foundSurveyLink) && !isAdmin) {
+    if (isBotAdmin) {
+      var groupLink = "https://chat.whatsapp.com/" + (await this.groupInviteCode(msg.chat));
+      if (msg.text.includes(groupLink)) return true;
+    }
+    if (isBotAdmin && botSettings.restrict) {
+      var warningMessage = {
+        'key': {
+          'participants': "0@s.whatsapp.net",
+          'fromMe': false,
+          'id': "Halo"
+        },
+        'message': {
+          'locationMessage': {
+            'name': "𝐀𝐧𝐭𝐢 - 𝐋𝐢𝐧𝐤",
+            'jpegThumbnail': await (await fetch("https://telegra.ph/file/a3b727e38149464863380.png")).buffer(),
+            'vcard': "BEGIN:VCARD\nVERSION:3.0\nN:;Unlimited;;;\nFN:Unlimited\nORG:Unlimited\nTITLE:\nitem1.TEL;waid=19709001746:+1 (970) 900-1746\nitem1.X-ABLabel:Unlimited\nX-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:Unlimited\nEND:VCARD"
+          }
+        },
+        'participant': "0@s.whatsapp.net"
+      };
+      conn.reply(msg.chat, "⚠ 𝐋𝐈𝐍𝐊 𝐃𝐈 𝐀𝐋𝐓𝐑𝐈 𝐆𝐑𝐔𝐏𝐏𝐈 𝐍𝐎𝐍 𝐒𝐎𝐍𝐎 𝐂𝐎𝐍𝐒𝐄𝐍𝐓𝐈𝐓𝐈", warningMessage);
+      await conn.sendMessage(msg.chat, { 'delete': { 'remoteJid': msg.chat, 'fromMe': false, 'id': messageId, 'participant': sender } });
+    }
   }
-};
-
-handler.command = /^(freestyle)$/i;  // Comando 
-handler.group = true;  // Funziona solo nei gruppi
-handler.admin = true;  // Solo per amministratori
-handler.botAdmin = true;  // Il bot deve essere admin nel gruppo
-
-export default handler;
+}
+```
